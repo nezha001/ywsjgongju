01fb6c3d1f6d3d27b6b748b22fd2f07326a89687
nezha001 <109815685+nezha001@users.noreply.github.com>
2023-10-25 00:08:04 +0800

    Update gongju.sh

--- gongju.sh
+++ gongju.sh
@@ -92,7 +92,7 @@ installssh() {
 }
 #进入云大师
 intoyun() {
-    rm -rf ./yundashi.sh && yum -y install wget && wget https://alist.yyzq.cf/d/%20%E6%9C%AC%E5%9C%B0%E7%BD%91%E7%9B%98/sh/yundashi.sh && chmod 777 ./yundashi.sh && bash ./yundashi.sh ; rm -rf ./yundashi.sh
+    rm -rf ./yundashi.sh && yum -y install wget && wget https://github.com/nezha001/ywsjgongju/blob/main/yundashi.sh && chmod 777 ./yundashi.sh && bash ./yundashi.sh ; rm -rf ./yundashi.sh
 }
 #安装frp内网穿透
 installfrp() {
