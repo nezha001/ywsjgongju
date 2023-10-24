4e067f2a3efe7e3d69007c79f5cf219a08897109
nezha001 <109815685+nezha001@users.noreply.github.com>
2023-10-25 00:15:00 +0800

    Add files via upload

---
+++ installumami.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo    
+sudo yum-config-manager --enable docker-ce-nightly
+sudo yum install docker-ce docker-ce-cli containerd.io
+systemctl start docker  #启动容器
+systemctl enable docker #开机自启
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker --version
+docker-compose --version
+echo "恭喜docker与docker-compose安装成功"
+yum -y install git curl wget
+mkdir /root/umami; cd /root/umami
+git clone https://github.com/mikecao/umami.git
+cd umami/
+docker-compose up -d
+ipv4aa=$(curl ifconfig.me)
+echo "安装成功此时访问
+http://$ipv4aa:3000
+即可进入umami程序，
+默认用户名admin 默认密码umami"
