6d666399e00161d3d6d378684390dc9653e63cc0
nezha001 <109815685+nezha001@users.noreply.github.com>
2023-10-25 00:13:18 +0800

    Add files via upload

---
+++ installmysql.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+rpm  -q  mariadb-server  mariadb
+systemctl  stop  mariadb 
+rpm  -e  --nodeps  mariadb  mariadb-server 
+rm  -rf  /etc/my.cnf  
+rm  -rf  /var/lib/mysql/*
+yum -y install wget && wget http://152.67.98.47:8889/down/NFGtj3Ix1wKk
+tar  -xf mysql-5.7.17.tar
+yum -y install mysql-community-*.rpm
+systemctl  start mysqld
+systemctl  enable mysqld
+netstat  -utnlp  | grep  3306
+ps -C mysqld
+grep password  /var/log/mysqld.log | tail -1
+
+
