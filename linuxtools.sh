3615bac9aec03c041fab90a35b15abb0eb9ebd48
nezha001 <109815685+nezha001@users.noreply.github.com>
2023-10-25 00:33:02 +0800

    Update linuxtools.sh

--- linuxtools.sh
+++ linuxtools.sh
@@ -8,19 +8,19 @@ if [[ -f /etc/redhat-release ]]; then
     bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
 elif cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
-   bash <(curl -k  https://alist.ywsj.cf/d/Cloudreve/shell/ubuntu/linux-ubuntu.sh)
+   bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
 elif cat /etc/issue | grep -Eqi "ubuntu"; then
     release="ubuntu"
-    bash <(curl -k  https://alist.ywsj.cf/d/Cloudreve/shell/ubuntu/linux-ubuntu.sh)
+    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
-    bash <(curl -k https://alist.ywsj.cf/d/share/sh/gongju.sh?sign=WrOJROwX3jpyVkLPwzBWrvIZnA-rmcLKuljqP3wYNw0=:0)
+    bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
 elif cat /proc/version | grep -Eqi "debian"; then
     release="debian"
-    bash <(curl -k  https://alist.ywsj.cf/d/Cloudreve/shell/ubuntu/linux-ubuntu.sh)
+    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
 elif cat /proc/version | grep -Eqi "ubuntu"; then
     release="ubuntu"
-    bash <(curl -k  https://alist.ywsj.cf/d/Cloudreve/shell/ubuntu/linux-ubuntu.sh)
+    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
     bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
