a249bbafeee32106d5c0902ef9c554692b9ed43e
nezha001 <109815685+nezha001@users.noreply.github.com>
2023-10-25 11:03:27 +0800

    Update linuxtools.sh

--- linuxtools.sh
+++ linuxtools.sh
@@ -5,25 +5,25 @@
 # check os
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
-    bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
+    bash <(curl -k https://raw.githubusercontent.com/nezha001/ywsjgongju/main/gongju.sh)
 elif cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
-   bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
+   bash <(curl -k  https://raw.githubusercontent.com/nezha001/ywsjgongju/main/linux-ubuntu.sh)
 elif cat /etc/issue | grep -Eqi "ubuntu"; then
     release="ubuntu"
-    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
+    bash <(curl -k  https://raw.githubusercontent.com/nezha001/ywsjgongju/main/linux-ubuntu.sh)
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
-    bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
+    bash <(curl -k https://raw.githubusercontent.com/nezha001/ywsjgongju/main/gongju.sh)
 elif cat /proc/version | grep -Eqi "debian"; then
     release="debian"
-    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
+    bash <(curl -k  https://raw.githubusercontent.com/nezha001/ywsjgongju/main/linux-ubuntu.sh)
 elif cat /proc/version | grep -Eqi "ubuntu"; then
     release="ubuntu"
-    bash <(curl -k  https://github.com/nezha001/ywsjgongju/blob/main/linux-ubuntu.sh)
+    bash <(curl -k  https://raw.githubusercontent.com/nezha001/ywsjgongju/main/linux-ubuntu.sh)
 elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
-    bash <(curl -k https://github.com/nezha001/ywsjgongju/blob/main/gongju.sh)
+    bash <(curl -k https://raw.githubusercontent.com/nezha001/ywsjgongju/main/gongju.sh)
 else
     echo -e "${red}未检测到系统版本，请联系脚本作者！运维世界www.ywsj.cf${plain}\n" && exit 1
 fi
